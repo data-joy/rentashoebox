c209396b07e6807cbd961232a011793ad9f6c7ee
Wilson So <103joy@gmail.com>
2015-01-21 18:55:20 -0800

    Add some helper scripts

--- scraper/run_indexer.sh
+++ scraper/run_indexer.sh
@@ -1,3 +1,4 @@
 python indexer.py -g eby -c fremont -n 
 python indexer.py -g eby -c fremont -l 3 -r ./data/index/eby_fremont_20150119_165027_1.html
 python indexer.py -g eby -c fremont -l 10
+python indexer.py -g sfc -d ../data/ -y 3
